5577c1f0cf2b74e884528d9ddb41a2de2e946f18
XuQiao <qixu@cern.ch>
2014-11-10 12:02:05 -0600

    new gamma fit

--- dNchdeta/PythiaMC/plot.C
+++ dNchdeta/PythiaMC/plot.C
@@ -1,34 +1,44 @@
 double function(double *v, double *par){
 	TF1 *gamma = new TF1("gamma","TMath::GammaDist(x,[0],0,[1])",0,100);
-	TF1 *gaus = new TF1("gaus","[0]*TMath::Gaus(x,[1]-[2],[3])",0,100);
+//	TF1 *gaus = new TF1("gaus","[0]*TMath::Gaus(x,[1]-[2],[3])",0,100);
 	gamma->SetParameters(par[0],par[1]);
-	gaus->SetParameters(par[2],v[0],par[3],par[4]);
-	TF1 *conv = new TF1("conv","gamma*gaus",0,100);
-	return conv->Integral(0,v[0]);
+//	gaus->SetParameters(par[2],v[0],par[3],par[4]);
+//	TF1 *conv = new TF1("conv","gamma*gaus",0,100);
+//	return conv->Integral(0,v[0]);
+	return gamma->Eval(v[0]);
 }
 
 void plot(){
+	gStyle->SetOptFit(kTRUE);
+	double xmin=1;
+	double xmax=50;
 	TFile *f = TFile::Open("/lio/lfs/cms/store/user/qixu/dNchdeta/MC/pythia6/pPbMC.root");
 	TDirectory *dir = f->GetDirectory("demo");
 	TH1D* hHFp4 = dir->Get("hHFp4");
 	TH1D* hHFp = dir->Get("hHFp");
-//	hHFp4->Rebin(10);
-//	hHFp->Rebin(10);
-	hHFp4->Scale(1.0/hHFp4->Integral());
-	hHFp->Scale(1.0/hHFp->Integral());
+	TH1D* hHF4 = dir->Get("hHF4");
+	TH1D* hHF3 = dir->Get("hHF3");
+	hHFp4->Scale(1.0/hHFp4->Integral("width"));
+	hHFp->Scale(1.0/hHFp->Integral("width"));
+	hHF4->Scale(1.0/hHF4->Integral("width"));
+	hHF3->Scale(1.0/hHF3->Integral("width"));
 	hHFp4->GetXaxis()->SetRangeUser(0,50);
 	hHFp->GetXaxis()->SetRangeUser(0,50);
+	hHF4->GetXaxis()->SetRangeUser(0,50);
 	TCanvas *c1= new TCanvas;
 	c1->SetLogy();
-	TF1 *g = new TF1("g",function,1,100,5);
-	g->SetParameters(2,3,0.1,0,1);
+	TF1 *g = new TF1("g",function,0,100,2);
+	g->SetParameters(0.8,2);
 //	hHFp->Fit("g");
-	hHFp4->Fit("g","","",0,100);
+	hHF4->Fit("g","","",xmin,xmax);
+//	hHFp4->Draw();
+//	g->Draw("same");
+	TLatex *tl = new TLatex(0.2,0.6,Form("fit range %.1f to %.1f",xmin,xmax));
 	c1->Print("hHFp4FitGamma.png");
-	cout<<g->GetParameter(0)<<"\t"<<g->GetParError(0)<<endl;
-	cout<<g->GetParameter(1)<<"\t"<<g->GetParError(1)<<endl;
-	cout<<g->GetParameter(2)<<"\t"<<g->GetParError(2)<<endl;
-	cout<<g->GetParameter(3)<<"\t"<<g->GetParError(3)<<endl;
-	cout<<g->GetParameter(4)<<"\t"<<g->GetParError(4)<<endl;
-	cout<<g->GetNDF()<<"\t"<<g->GetChisquare()<<endl;
+//	cout<<g->GetParameter(0)<<"\t"<<g->GetParError(0)<<endl;
+//	cout<<g->GetParameter(1)<<"\t"<<g->GetParError(1)<<endl;
+//	cout<<g->GetParameter(2)<<"\t"<<g->GetParError(2)<<endl;
+//	cout<<g->GetParameter(3)<<"\t"<<g->GetParError(3)<<endl;
+//	cout<<g->GetParameter(4)<<"\t"<<g->GetParError(4)<<endl;
+//	cout<<g->GetNDF()<<"\t"<<g->GetChisquare()<<endl;
 }
