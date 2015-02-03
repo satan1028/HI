3e50aac49220cf035b1acb2a848c9fa3fcd215c0
XuQiao <qixu@cern.ch>
2015-02-03 02:26:08 -0600

    debug for generating functin

--- flow/LYZ/v2/theta10/tracknormcpt03to6/M300260/plotgvsr.C
+++ flow/LYZ/v2/theta10/tracknormcpt03to6/M300260/plotgvsr.C
@@ -1,41 +1,62 @@
 #include "par.h"
 
-void plotgvsr(){
-
 int xbin=0;	//xbin<1
-int xtheta=0;	//xtheta<5
 double ptmin=0.3, ptmax=6.0;
+const int nnu = 2;
+const int marker[nnu] = {20,24};
+const int color[nnu] = {1,2};
+const int plotnu[nnu]={2,5};
+void plotgvsr(){
+    gStyle->SetOptStat(kFALSE);
+TCanvas *c1 = new TCanvas();
+c1->SetLogy();
+TH1D* hFrame = new TH1D("","",1000,0,1);
+hFrame->GetYaxis()->SetRangeUser(5e-6,1e1);
+hFrame->GetXaxis()->SetRangeUser(0.1,0.32);
+hFrame->GetXaxis()->SetTitle("r");
+hFrame->GetYaxis()->SetTitle("|G^{#theta}(ir)|^{2}");
+hFrame->SetTitle("");
+hFrame->Draw();
+TGraph *gr[nnu];
+for(int itheta=0;itheta<nnu;itheta++){
+gr[itheta]=plotGF(0,plotnu[itheta],marker[itheta],color[itheta]);
+gr[itheta]->Draw("Psame");
+}
+TLegend *tg = new TLegend(0.75,0.70-0.10*nnu,0.90,0.70);
+tg->SetFillColor(0);
+tg->SetBorderSize(0);
+tg->SetTextSize(0.04);
+for(int itheta=0;itheta<nnu;itheta++)
+tg->AddEntry(gr[itheta],Form("#theta = #frac{%d}{%d}*#frac{#pi}{%d}",plotnu[itheta],ntheta,nn),"lp");
+tg->Draw("same");
+TLatex *t= new TLatex();
+t->SetNDC();
+t->SetTextSize(0.03);
+t->SetTextFont(42);
+t->DrawLatex(0.5,0.2,Form("track, %d < mult <%d, %.1f < p_{T} < %.1f", trkbin[xbin+1],trkbin[xbin],ptmin,ptmax));
 
+c1->Print("gvsr_thetas.png");
+
+
+}
+
+TGraph *plotGF(int isSum, int xtheta, int marker, int color){
+
+if(isSum)
 TFile *f = TFile::Open("mergedV_Sum.root");
-TVectorD *vecDr = f->Get(Form("D_%d/r",xbin));
-TVectorD *vecDg2 = f->Get(Form("D_%d_%d/G2",xbin,xtheta));
+else
+TFile *f = TFile::Open("mergedV_Prod.root");
 
+TVectorD *vecDr = f->Get(Form("D_%d/r",xbin));
+TVectorD *vecDg2 = f->Get(Form("D_%d/D_0/D_%d/G2",xbin,xtheta));
 
 double *r = vecDr->GetMatrixArray();
 double *g2 = vecDg2->GetMatrixArray();
-
-TCanvas *c1 = new TCanvas;
-c1->SetLogy();
-TGraph *gr=new TGraph(nstepr,r,g2);
-gr->GetYaxis()->SetRangeUser(5e-7,1e-3);
-gr->GetXaxis()->SetRangeUser(0.15,0.4);
-gr->GetXaxis()->SetTitle("r");
-gr->GetYaxis()->SetTitle("|G^{#theta}(ir)|^{2}");
-gr->SetTitle("LYZ Sum");
-gr->SetMarkerSize(1);
-gr->SetMarkerColor(1);
-gr->SetMarkerStyle(20);
-gr->Draw("AP");
-TLatex *t= new TLatex();
-t->SetNDC();
-if(xtheta==0)
-t->DrawLatex(0.3,0.8,Form("%d < mult <%d, #theta = %d, %.1f < p_{T} < %.1f", trkbin[xbin+1],trkbin[xbin],xtheta,ptmin,ptmax));
-else
-t->DrawLatex(0.3,0.8,Form("%d < mult <%d, #theta = #frac{%d}{%d}#pi, %.1f < p_{T} < %.1f", trkbin[xbin+1],trkbin[xbin],xtheta,ntheta*nn,ptmin,ptmax));
-
-c1->Print("gvsr.png");
-
+TGraph *gr = new TGraph(nstepr,r,g2);
+gr->SetMarkerSize(0.5);
+gr->SetMarkerColor(color);
+gr->SetMarkerStyle(marker);
 f->Close();
-
+return gr;
 }
 
