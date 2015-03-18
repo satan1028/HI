9c4798247f17a3576814a9f16c7300cc3c608c49
XuQiao <qixu@cern.ch>
2015-03-17 23:52:37 -0500

    MHAT wavelet

--- flow/wavelet/MHATfunc.C
+++ flow/wavelet/MHATfunc.C
@@ -4,24 +4,30 @@ void MHATfunc(){
     TTree *t = (TTree*)f->Get("tree");
     const int nbinx=100;
     const int nbiny=100;
-    Float_t eta[1000],phi[1000];
-    int n;
+    Float_t eta[1000]={1,.275,3.25,5,6,7};
+    Float_t phi[1000];
+    int n=6;
     t->SetBranchAddress("etag",eta);
     t->SetBranchAddress("phig",phi);
     t->SetBranchAddress("n",&n);
     Int_t N = t->GetEntries();
     double W[nbinx][nbiny] = {};
     double a[nbinx], b[nbiny];
-    for(int ia=0; ia<2;ia+=0.02){
-    	for(int ib=-3; ib<3;ib+=0.06){
-    for(int ievt=0;ievt<N;ievt++){
-        t->GetEntry(ievt);
+    TGraph2D* gr2d = new TGraph2D(nbinx*nbiny);
+    int i=0;
+    for(double ia=0.02; ia<=2;ia+=0.02){
+        int j=0;
+    	for(double ib=-3; ib<3;ib+=0.06){
+    for(int ievt=0;ievt<1;ievt++){
+ //   if(ievt%10000==0)        cout<<"Processing "<<ievt<<" events"<<endl;
+  //      t->GetEntry(ievt);
        for(int imult = 0;imult<n; imult++){
     //    h->Fill(eta[imult],phi[imult]);
     	W[i][j] += 1./n*TMath::Sqrt(1./a)*MHATg2((eta[imult]-b)/a);
        }
     }
-    W[i][j]/=N;
+//    W[i][j]/=N;
+    gr2d->SetPoint(j+i*nbiny,ia,ib,W[i][j]);
     b[j] = ib;
     j++;
     }
@@ -30,7 +36,6 @@ void MHATfunc(){
     i++;
     }
 
-    TGraph2D* gr2d = new TGraph2D(nbinx*nbiny,a,b,W);
     gr2d->Draw("surf1");
 
 }
