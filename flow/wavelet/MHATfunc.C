f3ae1deaf895b3410cdb6aade69bcd90887ff944
XuQiao <xu.qiao93@gmail.com>
2015-03-26 23:19:57 -0500

    wavelet

--- flow/wavelet/MHATfunc.C
+++ flow/wavelet/MHATfunc.C
@@ -15,22 +15,6 @@ double MHATg4(double);
 
 void MHATfunc(){
     int ifile=0;
-<<<<<<< HEAD
-     TFile *f = TFile::Open(Form("/cms/store/user/qixu/flow/STEG/pPbDataV200m100/vndata_50k_%d.root",ifile));
-    TTree *t = (TTree*)f->Get("tree");
-    const int nbinx=100;
-    const int nbiny=100;
-    Float_t eta[1000]={1,2.75,3.25,5,6,7};
-    Float_t phi[1000];
-    int n=6;
-//    t->SetBranchAddress("etag",eta);
-//    t->SetBranchAddress("phig",phi);
-//    t->SetBranchAddress("n",&n);
-//    Int_t N = t->GetEntries();
-    double W[nbinx][nbiny] = {};
-    double a[nbinx], b[nbiny];
-    TGraph2D* gr2d = new TGraph2D(nbinx*nbiny);
-=======
     TFile *f = TFile::Open(Form("/cms/store/user/qixu/flow/NewSTEG/pPbDataV205m100/vndata_50k_%d.root",ifile));
     TTree *t = (TTree*)f->Get("tree");
     TFile *fout = new TFile("testV205m100.root","Recreate");
@@ -69,33 +53,19 @@ void MHATfunc(){
     TH1D* hEWg21D = new TH1D("hMHAThg2EW","",nbinx,amin,amax);
     TH1D* hEWg41D = new TH1D("hMHAThg4EW","",nbinx,amin,amax);
 //    grEW->SetName("MHATg2E");
->>>>>>> a7390aab15ce8aeb25f6edadbe500fc832df2b2d
     int i=0;
     for(double ia=amin; ia<=amax;ia+=astep){
-        int j=0;
-<<<<<<< HEAD
-    	for(double ib=0; ib<8;ib+=0.08){
-    for(int ievt=0;ievt<1;ievt++){
- //   if(ievt%10000==0)        cout<<"Processing "<<ievt<<" events"<<endl;
-  //      t->GetEntry(ievt);
-       for(int imult = 0;imult<n; imult++){
-    //    h->Fill(eta[imult],phi[imult]);
-    	W[i][j] += 1./n*TMath::Sqrt(1./ia)*MHATg2((eta[imult]-ib)/ia);
-=======
         if(ia==0)continue;
-        if(ia!=a)continue;
-    	for(double ib=bmin; ib<=bmax;ib+=bstep){
-    for(int ievt=0;ievt<N;ievt++){
-    if(ievt%50000==0)        cout<<"Processing "<<ievt<<" events"<<endl;
-      t->GetEntry(ievt);
-       for(int imult = 0;imult<n; imult++){
+       // if(ia!=a)continue;
+        int j=0;
+        for(double ib=bmin; ib<=bmax;ib+=bstep){
+        for(int imult = 0;imult<n; imult++){
     //    h->Fill(eta[imult],phi[imult]);
         double x = (phi[imult]-ib)/ia;
     	Wg2[i][j] += 1./n*TMath::Sqrt(1./ia)*MHATg2(x);
     	W2g2[i][j] += TMath::Power(1./n*TMath::Sqrt(1./ia)*MHATg2(x),2);
     	Wg4[i][j] += 1./n*TMath::Sqrt(1./ia)*MHATg4(x);
     	W2g4[i][j] += TMath::Power(1./n*TMath::Sqrt(1./ia)*MHATg4(x),2);
->>>>>>> a7390aab15ce8aeb25f6edadbe500fc832df2b2d
        }
     }
     Wg2[i][j]/=N;
@@ -122,11 +92,7 @@ void MHATfunc(){
     cout<<i<<endl;
     i++;
     }
-<<<<<<< HEAD
 
-    gr2d->Draw("colz");
-
-=======
     fout->cd();
     hWg22D->Write();
     hW2g22D->Write();
@@ -138,7 +104,6 @@ void MHATfunc(){
     hWg21D->Write();
     hWg41D->Write();
     fout->Close();
->>>>>>> a7390aab15ce8aeb25f6edadbe500fc832df2b2d
 }
 
 double MHATg1(double x){
